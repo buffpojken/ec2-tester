37e5fc25428a4950fbef7612087aecac87a5be46
buffpojken <daniel@sykewarrior.com>
2011-01-20 17:08:39 +0100

    added logic

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -3,7 +3,8 @@ class PublicController < ApplicationController
   def index
     @k = Negerkung.create(:name => rand(10000).to_s)
     @kings = Negerkung.find(:all)
-    render :text => "#{@kings.length.to_s} negerkungar skapade"
+    
+    render :text => "#{@kings.length.to_s} negerkungar skapade, with #{@k.name.to_s} being the latest one"
   end
     
     
